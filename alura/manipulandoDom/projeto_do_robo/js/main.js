a5a1c774f8732ce19f465909a932036650331b6f
Gabriel Roger <122801172+GabrielRogerDelano@users.noreply.github.com>
2023-04-15 16:38:02 -0300

    organizando cursos

---
+++ alura/manipulandoDom/projeto_do_robo/js/main.js
@@ -0,0 +1,10 @@
+const robotron = document.querySelector('#robotron')
+
+robotron.addEventListener("click", () => {
+    alert("cliquei no robô")
+})
+
+function oi(){
+    alert("oi")
+    alert("Bem vindo")
+}
