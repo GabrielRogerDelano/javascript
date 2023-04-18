f324883778e6ea7426bfde07af4d5df7da2a1bbc
Gabriel Roger <122801172+GabrielRogerDelano@users.noreply.github.com>
2023-04-18 08:17:44 -0300

    finalizando projeto do robo

---
+++ alura/laços e listas/script.js
@@ -0,0 +1,10 @@
+let imagemDaEstrada
+function preload(){
+    imagemDaEstrada = loadImage("material/img/estrada.png")
+}
+function setup(){
+    createCanvas(500,400)
+}
+function draw(){
+    background(imagemDaEstrada)
+}
