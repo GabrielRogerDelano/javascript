e51316b924bd5a7c991f87292011007fe3d59f04
Gabriel Roger <122801172+GabrielRogerDelano@users.noreply.github.com>
2023-04-28 08:07:51 -0300

    Update index.js

--- alura/POO/index.js
+++ alura/POO/index.js
@@ -1,20 +1,35 @@
 class cliente{
     nome;
     cpf;
+    
+}
+
+class contaCorrente{
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo > valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new cliente();
-
 cliente1.nome = "Gabriel";
 cliente1.cpf = 11223344556;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
-const cliente2Nome = "alice";
-const cliente2CPF = 10000001;
-const cliente2Agencia = 900;
-const cliente2saldo = 0;
+const cliente2 = new cliente();
+cliente2.nome = "alice";
+cliente2.cpf = 10000001;
+
+const contaCorrenteRicardo = new contaCorrente();
+contaCorrenteRicardo.saldo = 120;
+contaCorrenteRicardo.agencia = 1001;
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(100)
+console.log(contaCorrenteRicardo.saldo);
 
 console.log(cliente1)
+console.log(cliente2)
