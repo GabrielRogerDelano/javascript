a1f95c23619af83d6d4e267d820d78db1312f047
Gabriel Roger <122801172+GabrielRogerDelano@users.noreply.github.com>
2023-04-27 21:47:48 -0300

    Create index.js

---
+++ alura/POO/index.js
@@ -0,0 +1,20 @@
+class cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new cliente();
+
+cliente1.nome = "Gabriel";
+cliente1.cpf = 11223344556;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+const cliente2Nome = "alice";
+const cliente2CPF = 10000001;
+const cliente2Agencia = 900;
+const cliente2saldo = 0;
+
+console.log(cliente1)
